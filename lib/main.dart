ca68c060ad294d1771e8115a13865b9fecb4e9a3
Santy-R-cell <riveradiegosantiago343@gmail.com>
2024-09-21 22:23:10 -0600

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -259,3 +259,38 @@ title:  Text("Inicio", style: TextStyle(color: Colors.white),),
     );
   }
 }
+
+// Pantalla de Perfil  
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+     title:  Text("Perfil", style: TextStyle(color: Colors.white),),
+        backgroundColor: const Color.fromARGB(255, 0, 171, 251),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 60,
+            //  backgroundImage: AssetImage('assets/profile_pic.png'),
+            backgroundImage: NetworkImage('https://cdn-icons-png.flaticon.com/512/3135/3135768.png'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Nombre del Usuario',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Insumos BR',
+              style: TextStyle(fontSize: 18, color: Colors.grey[700]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
