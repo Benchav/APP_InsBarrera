427f7f692c16986b705d84440680da6de7a0787c
Benchav <joshua44benja@gmail.com>
2024-11-04 22:23:20 -0600

    a

--- lib/screens/pastry_products_screen.dart
+++ lib/screens/pastry_products_screen.dart
@@ -6,8 +6,8 @@ class PastryProductsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('"Bienvenido a la app de InsumosBr"'),
-        backgroundColor: const Color.fromARGB(255, 0, 171, 251),
+        title: Text('"Bienvenido a la app de InsumosBr"',style: TextStyle(color: Colors.white)),
+        backgroundColor: const Color.fromARGB(255, 26, 171, 238),
         automaticallyImplyLeading: false, // Oculta el botón de regreso
       ),
       body: SingleChildScrollView(
