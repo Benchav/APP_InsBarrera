dd3c9887d62eb064150d377515432ef0c8cef647
Benchav <joshua44benja@gmail.com>
2024-11-16 16:32:30 -0600

    nv

--- lib/screens/pastry_products_screen.dart
+++ lib/screens/pastry_products_screen.dart
@@ -6,7 +6,7 @@ class PastryProductsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('"Bienvenido usuario"',style: TextStyle(color: Colors.white)),
+        title: Text('"Bienvenido usuario"', style: TextStyle(color: Colors.white)),
         backgroundColor: const Color.fromARGB(255, 26, 171, 238),
         automaticallyImplyLeading: false, // Oculta el botón de regreso
       ),
@@ -17,62 +17,52 @@ class PastryProductsScreen extends StatelessWidget {
             children: [
               TableRow(children: [
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
+                  imageUrl: 'https://bhgsuper.es/178-large_default/harina-de-trigo-espelta-bio-1kg-el-granero-integral.jpg',
                   text: 'Harina',
                 ),
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
+                  imageUrl: 'https://www.oetker.es/assets/hygraph/output=format:webp/resize=fit:clip,height:662,width:662/quality=value:75/compress/Km3OZrWBSL2RPeiulZY1',
                   text: 'Levadura',
                 ),
               ]),
               TableRow(children: [
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
+                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTrKGOlLfSD1U3U8JFjmCOUxtRbiHRGoJ7SA&s',
                   text: 'Mantequilla',
                 ),
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
-                  text: 'Azúcar',
+                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg_gRr5CrN2PcndKaCQh6TGK7WOVqEfktInw&s',
+                  text: 'Azúcar Morena',
                 ),
               ]),
               TableRow(children: [
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
+                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyF1BR0BksKEsUkW16ABhYeE__DMtk8K_pCA&s',
                   text: 'Colorante',
                 ),
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
+                  imageUrl: 'https://www.lala.com.mx/storage/app/media/Prodcutos/7501020513134.png',
                   text: 'Crema',
                 ),
               ]),
               TableRow(children: [
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
+                  imageUrl: 'https://www.lacentraldeabasto.com/1417-large_default/azucar-glass-zulka-500-g.jpg',
                   text: 'Azúcar Glass',
                 ),
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
+                  imageUrl: 'https://static.wixstatic.com/media/868e30_adf6388995d94d5381e5a79261377592~mv2.png/v1/fit/w_500,h_500,q_90/file.png',
                   text: 'Chispas',
                 ),
               ]),
               TableRow(children: [
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
+                  imageUrl: 'https://manitoba.com.co/storage/2021/06/Mani-con-sal.png',
                   text: 'Maní',
                 ),
                 _SingleCard(
-                  color: Colors.white,
-                  icon: Icons.cake_rounded,
-                  text: 'Rikitiksi',
+                  imageUrl: 'https://delmontefrutas.mx/wp-content/uploads/2021/07/Cereza-Negras-454g.png',
+                  text: 'Cereza',
                 ),
               ]),
             ],
@@ -84,12 +74,14 @@ class PastryProductsScreen extends StatelessWidget {
 }
 
 class _SingleCard extends StatelessWidget {
-  final IconData icon;
-  final Color color;
+  final String imageUrl;
   final String text;
 
-  const _SingleCard(
-      {super.key, required this.icon, required this.color, required this.text});
+  const _SingleCard({
+    super.key, 
+    required this.imageUrl, 
+    required this.text,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -97,18 +89,20 @@ class _SingleCard extends StatelessWidget {
       margin: EdgeInsets.all(15),
       height: 180,
       decoration: BoxDecoration(
-          color: Color.fromRGBO(82, 183, 230, 0.6),
-          borderRadius: BorderRadius.circular(20)),
+        color: Color.fromRGBO(82, 183, 230, 0.6),
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          CircleAvatar(
-            backgroundColor: this.color,
-            child: Icon(
-              this.icon,
-              size: 35,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Image.network(
+              this.imageUrl,
+              height: 60,
+              width: 60,
+              fit: BoxFit.cover,
             ),
-            radius: 30,
           ),
           SizedBox(
             height: 10,
@@ -116,7 +110,7 @@ class _SingleCard extends StatelessWidget {
           Text(
             this.text,
             style: TextStyle(color: Colors.black, fontSize: 18),
-          )
+          ),
         ],
       ),
     );
